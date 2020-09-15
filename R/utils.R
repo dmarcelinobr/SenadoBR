1bf8fc01a787de1d540bac2faff1c54e9a4ddd85
daniel <dmarcelino@live.com>
2020-09-14 22:53:02 -0300

    adjusting for R4

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,7 @@
 
 url_exists <- function(url){
   tryCatch(
-    identical(status_code(HEAD(url)),200L), 
+    identical(httr::status_code(HEAD(url)),200L), 
     error = function(e){
       FALSE
     })
